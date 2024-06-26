fc51428fbcf5f34cd5300b922e27de19041d1334
Lpik97 <dm97fx@gmail.com>
2024-06-26 08:54:45 +0200

    feat(project): initialize project with Express generator and EJS
    Set up the project using Express generator with EJS as the view engine for rendering.
    
    - Initialized project structure and dependencies using Express generator
    - Configured EJS as the view engine for rendering dynamic content

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import createError from "http-errors";
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import mongoDB from "./config/database.js";
+import mongoose from "mongoose";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// mongoose connection to the mongoDB
+mongoose.set("strictQuery", false);
+main().catch((err) => console.log(err));
+async function main() {
+   await mongoose.connect(mongoDB);
+}
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use(function (req, res, next) {
+   next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+   res.locals.message = err.message;
+   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+   res.status(err.status || 500);
+   res.render("error");
+});
+
+export default app;
