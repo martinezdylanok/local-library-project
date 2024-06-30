d1442663881417b595a074a2d7b86dd6975e1cf0
Lpik97 <dm97fx@gmail.com>
2024-06-30 12:05:09 +0200

    refactor(router): update root route to redirect to /catalog

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-   res.render("index", { title: "Express" });
+router.get("/", function (req, res) {
+   res.redirect("/catalog");
 });
 
 export default router;
