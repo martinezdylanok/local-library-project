aca6778c5756ff643d270474d2bb11725f2b4073
Lpik97 <dm97fx@gmail.com>
2024-07-01 09:47:34 +0200

    feat: refactor homepage rendering and implement book list
    - Refactored the index function to render the homepage using the "index" template.
    - Implemented book_list function to display a list of all books sorted alphabetically by title and populated with author information.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -7,7 +7,7 @@ import asyncHandler from "express-async-handler";
 export const index = asyncHandler(async (req, res, next) => {
    const [numBooks, numBookInstances, numAvailableBookInstances, numAuthors, numGenres] = await Promise.all([Book.countDocuments({}).exec(), BookInstance.countDocuments({}).exec(), BookInstance.countDocuments({ status: "Available" }).exec(), Author.countDocuments({}).exec(), Genre.countDocuments({}).exec()]);
 
-   res.render("layout", {
+   res.render("index", {
       title: "Local Library Home",
       book_count: numBooks,
       book_instance_count: numBookInstances,
@@ -19,7 +19,9 @@ export const index = asyncHandler(async (req, res, next) => {
 
 // Display list of all books.
 export const book_list = asyncHandler(async (req, res, next) => {
-   res.send("NOT IMPLEMENTED: Book list");
+   const allBooks = await Book.find({}, "title author)").sort({ title: 1 }).populate("author").exec();
+
+   res.render("book_list", { title: "Book List", book_list: allBooks });
 });
 
 // Display detail page for a specific book.
