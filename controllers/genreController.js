25ce3893a9029dd5502920621d5cadc7964ab26b
Lpik97 <dm97fx@gmail.com>
2024-07-03 08:17:22 +0200

    feat(controller): implement genre list display
    Added implementation to display a list of all genres.
    Genres are sorted alphabetically by name.
    Rendered genre list view template with the sorted genres.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,11 @@ import asyncHandler from "express-async-handler";
 
 // Display list of all Genre.
 export const genre_list = asyncHandler(async (req, res, next) => {
-   res.send("NOT IMPLEMENTED: Genre list");
+   const allGenres = await Genre.find().sort({ name: 1 }).exec();
+   res.render("genre_list", {
+      title: "Genre List",
+      genre_list: allGenres,
+   });
 });
 
 // Display detail page for a specific Genre.
