4b16009e48563f66d8cd013cec7da243655abaa8
Lpik97 <dm97fx@gmail.com>
2024-07-09 09:01:54 +0200

    refactor: update MongoDB URI configuration to use environment variable
    - Replaced username and password variables with MONGODB_URI from environment variables.
    - Enhanced error handling by throwing an error if MONGODB_URI is missing from environment variables.

--- config/database.js
+++ config/database.js
@@ -2,14 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+const uri = process.env.MONGODB_URI;
 
-if (!username || !password) {
-   throw new Error("Missing USERNAME or PASSWORD in environment variables");
+if (!uri) {
+   throw new Error("Missing MONGODB_URI in environment variables");
 }
-
-const uri = `mongodb+srv://${username}:${password}@cluster0.trsdxew.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0
-`;
 
 export default uri;
